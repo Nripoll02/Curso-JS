0245e08df4c4835e2894d654bbe682b368772ac0
Nahuel Ripoll <nahuelripoll33@gmail.com>
2022-06-23 12:15:42 -0300

    Curso JS CH

---
+++ Clases/clase_1/js/main.js
@@ -0,0 +1,101 @@
+//CLASE 1
+// console.log("Hola Mundo");
+
+// console.log('Se inicio el js');
+
+// Declarar variables y constantes
+
+// let nombre = "Nahuel";
+// let precio =  4.8;
+// let precio2 = 5.3;
+// const dni = 44309329;
+
+// let saludo = 'hola';
+// console.log(saludo);
+// saludo = 'Hola mundo';
+// console.log(saludo);
+
+// console.log(dni);
+
+// let x = 15;
+// let y = 40;
+
+// let cartel1 = "Hola ";
+// let cartel2 = "mundo";
+
+// console.log(x+y);
+// console.log(cartel1 + cartel2);
+
+//Pedir datos al usuario
+// let nombre = prompt('Ingrese nombre');
+// let edad = prompt('Ingrese edad');
+
+// alert("Bienvenido " + nombre + " tu tienes: " + edad );
+
+// let a = prompt('Ingrese el valor de A');
+// parseInt(a);
+// let b = prompt('Ingrese el valor de B' );
+// parseInt(b);
+// let resultado = (a + b);
+// // alert('La persona se llama: ' + resultado);
+// alert('El resultado de la suma es: ' + resultado);
+
+/***************************************************************/
+//CLASE 2
+
+// console.log("Arranco el programa");
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+
+// let x = 2;
+// let nombre = "Nahuel";
+// let modoOscuro = true;
+
+// if(modoOscuro){
+//     console.log("Modo oscuro activado");
+// }else{
+//     console.log("Modo oscuro desactivado");
+// }
+
+// if(1 == 1){
+//     console.log("4");
+//     console.log("5");
+//     console.log("6");
+// }
+// console.log("7");
+// console.log("8");
+// console.log("9");
+
+// let x = 5;
+// let y = 5;
+// let w = 10;
+// let z = 10;
+
+// if (x === y && w === z) {
+//   console.log("Es correcto");
+// } else {
+//   console.log("No es correcto");
+// }
+
+// let a = 2;
+// let b = 10;
+// let c = 25;
+// let d = 25;
+
+// if (a === b || c === d) {
+//   console.log("Es correcto tambien");
+// } else {
+//   console.log("No es correcto");
+// }
+
+//EJERCITACIÓN DE PRÁCTICA
+
+let nombre = prompt("Ingrese su nombre");
+const apellido = prompt("Ingrese su apellido");
+let pais = prompt("Ingrese su pais de residencia");
+let edad = prompt("Ingrese su edad");
+
+console.log("Bienvenido "+nombre, "" +apellido, " Lindo pais "+pais, " Tu edad es: "+edad);
+
---
+++ Clases/clase_2/js/main.js
@@ -0,0 +1,17 @@
+let i = 0; 
+do { i++; console.log("Hola!"+i); } 
+while (i < 5);
+
+for (let i = 1; i < 13; i++) {
+    console.log("Mes: " + i);
+}
+
+
+let a = 0;
+let b = 0;
+while (b < 4) {
+  a++;
+  b += a;
+
+  console.log("El valor de a es: "+a);
+}
