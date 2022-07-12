3770be14752c042c9fa867c023733a9b5e629224
Nahuel Ripoll <nahuelripoll33@gmail.com>
2022-07-12 11:49:11 -0300

    Curso JS CH

---
+++ Clases/clase_8/js/main.js
@@ -0,0 +1,27 @@
+// setTimeout(() => {
+//      document.getElementById('myText').innerHTML = "Gracias por agregar algo al carro"
+// },2000)
+
+// let resaltados = document.getElementsByClassName('resalta');
+// console.log(resaltados[1]);
+
+
+// let nodos = document.getElementsByTagName('p');
+// console.log(nodos[0]);
+// console.log(nodos[1]);
+// console.log(nodos[2]);
+
+
+// let misP = document.getElementsByTagName('p');
+// console.log(misP);
+// setTimeout(() => {
+// for(const p of misP){
+//     p.style.color = 'red';
+//     p.style.fontSize = '30px';
+//     p.innerHTML = 'Hola mundo';
+// }
+// },2000)
+
+setTimeout(() => {
+    document.getElementById('p-vacio').innerHTML = '<p>Hola mundo</p><button>Holanda</button>';
+},2000)
--- Desafios/Simulador interactivo/js/main.js
+++ Desafios/Simulador interactivo/js/main.js
@@ -1,21 +1,48 @@
+let alumnos = [];
+function RegistrarAlumno(){
+    cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos que desea registrar"));
+    for(i=0; i<cantidadAlumnos; i++){
+        alumnos.push({nombre: prompt("Ingrese el nombre del alumno"),legajo: prompt("Ingrese el legajo del alumno")});
+    }
+}
+
+function MostrarAlumnos(){
+    for(i = 0; i < alumnos.length; i++){
+        console.log(alumnos[i].nombre, alumnos[i].legajo);
+    }
+}
+
+function BorrarAlumno(){
+    let legajo = prompt("Ingrese el legajo del alumno");
+    for(i = 0; i < alumnos.length; i++){
+        if(alumnos[i].legajo == legajo){
+            alumnos.splice(i,1);
+        }
+    }
+}
+
 function CalcularPromedio(){
-    
+
+    const aprobados = [];
+    const desaprobados = [];
     for(let alumnos = 1; alumnos <4; alumnos++){
         let parcial,final,promedio;
+        nombre = parseInt(prompt("Ingrese el nombre del alumno"));
         parcial = parseFloat(prompt("Parcial"));
         final = parseFloat(prompt("Final"));
         promedio = (parcial+final)/2;
     
         if(promedio >= 7){
-            document.write("El promedio es "+promedio+" APROBADO ");
+            document.write("El promedio de "+nombre+" es "+promedio+" APROBADO ");
+            aprobados.push(promedio);
+            console.log(aprobados);
         }else{
-            document.write("El promedio es "+promedio+" DESAPROBADO ");
+            document.write("El promedio de "+nombre+ " es "+promedio+" DESAPROBADO ");
+            desaprobados.push(promedio);
+            console.log(desaprobados);
         }
+    
     }
+    console.log(aprobados[1]);
 }
 
-// if(prom<7){
-//     console.log("Alumno desaprobado");
-// }else{
-//     console.log("Alumno aprobado");
-// }
