be78d3bb51da0ccaf7eca46ed3f479bd20370dcb
Nahuel Ripoll <nahuelripoll33@gmail.com>
2022-07-27 00:15:25 -0300

    Curso JS CH

---
+++ Clases/clase_12/js/main.js
@@ -0,0 +1,46 @@
+// let num = 10;
+
+// /*num =  num +1;*/
+// num += 1;
+// num -= 1;
+// console.log(num);
+
+// let temperatura = 25;
+
+// if (temperatura > 30) {
+//   console.log("Hace calor");
+// }else{
+//   console.log("Dia agradable");
+// }
+
+//IF MAS CORTO
+//SOLO PUEDO PONER UNA LINEA
+//OBLIGADO PONER UN ELSE
+
+// (temperatura > 30) ? console.log("Hace calor") : console.log("Dia agradable");
+//Condicion ? true : false;
+
+
+
+// const carrito = [];
+
+// if(carrito.length === 0){
+//   console.log("Vacio");
+// }
+
+// carrito.length === 0 && console.log("Vacio"); //OTRA FORMA DE HACERLO
+
+
+// function saludar(nombre){
+//     alert('Hola! '+ (nombre || 'Anonimo ') + 'Como estas?'); // || es el OR
+// }
+
+// const nombre = prompt("Cual es tu nombre?");
+// saludar(nombre);
+
+
+
+// let carrito;
+
+// let carritoLocalStorage = localStorage.getItem(('carrito') || []); // || es el OR
+
